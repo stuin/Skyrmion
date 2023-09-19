95490d0a0eacc6fa124b17a7d2f41bc2ea6e697f
stuin <stuin01@gmail.com>
2023-09-18 19:05:58 -0700

    Finished Vertex Graph

--- VertexGraph.hpp
+++ VertexGraph.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 using Edge = unsigned short;
 
 template<Edge size> 
@@ -37,69 +39,84 @@ public:
 		return root;
 	}
 
-	Vertex<size> *getSelected() {
-		return *selected;
-	}
-
 	Vertex<size> **_getSelectPointer() {
 		return selected;
 	}
 
-	Edge getOpposite(Edge edge) {
-		return (edge + size / 2) % size;
+	Vertex<size> *getSelected() {
+		return *selected;
 	}
 
-	Vertex<size> *select(Vertex<size> *vertex) {
-		if(vertex != NULL && selected == vertex->selected && *selected != vertex) {
-			(*selected)->select(false);
-			*selected = vertex;
-			(*selected)->select(true);
+	bool isSelected() {
+		return *selected == this;
+	}
+
+	Vertex<size> *getVertex(Edge edge) {
+		return edges[edge % size];
+	}
+
+	Vertex<size> *select() {
+		if(*selected != this) {
+			(*selected)->onSelect(false);
+			*selected = this;
+			(*selected)->onSelect(true);
 		}
+		//printAddress();
 		return *selected;
 	}
 
 	Vertex<size> *select(Edge edge) {
 		if(edges[edge % size] == NULL)
-			return select(this);
-		return select(edges[edge % size]);
+			return select();
+		return edges[edge % size]->select();
 	}
 
 	void setVertex(Edge edge, Vertex<size> *vertex) {
 		edges[edge % size] = vertex;
-		vertex->edges[getOpposite(edge)] = this;
 	}
 
-	void setOneWay(Edge edge, Vertex<size> *vertex) {
-		edges[edge % size] = vertex;
+	void setVertex(Edge edge, Vertex<size> *vertex, Edge opposite) {
+		setVertex(edge, vertex);
+		vertex->setVertex(opposite, this);
 	}
 
-	void addVertex(Edge edge, Vertex<size> *vertex) {
+	void addVertex(Edge edge, Vertex<size> *vertex, Edge opposite) {
 		if(vertex == NULL || selected != vertex->selected)
 			return;
 
 		edge = edge % size;
-		if(edges[edge] == NULL) {
-			edges[edge] = vertex;
-			vertex->edges[getOpposite(edge)] = this;
-		} else {
-			edges[edge]->addVertex(edge, vertex);
-		}
+		if(edges[edge] == NULL)
+			setVertex(edge, vertex, opposite);
+		else
+			edges[edge]->addVertex(edge, vertex, opposite);
 	}
 
-	void addVertex(Edge edge) {
-		addVertex(edge, new Vertex<size>(root));
+	void addVertex(Edge edge, Edge opposite) {
+		addVertex(edge, new Vertex<size>(root), opposite);
 	}
 
 	void printAddress() {
-		std::cout << getID() << ": [";
+		std::cout << displayName() << ": [";
 		for(Edge e = 0; e < size; e++) {
 			if(edges[e] != NULL)
-				std::cout << edges[e]->getID();
+				std::cout << edges[e]->displayName();
 			else
 				std::cout << "_";
 			std::cout << ",";
 		}
 		std::cout << "]\n";
+	}
+
+	void printEdge(Edge edge, Vertex<size> *vertex) {
+		std::cout << "\t" << displayName() << "->";
+		std::cout << vertex->displayName(); 
+		if(edge % 2 == 1)
+			std::cout << " [color = red]";
+		std::cout << "\n";
+	}
+
+	virtual std::string displayName() {
+		return std::to_string(getID());
 	}
 
 	virtual void onSelect(bool selected) {}
